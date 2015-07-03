243c11cea532e19c97ff2bd459ae98fe4ed7f93b
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-07-03 01:32:03 +0100

    User cannot sign-up with the wrong password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,12 +4,18 @@ feature 'User signs up' do
     expect(page).to have_content('Welcome, Guillaume@bouffard.com')
   end
 
+  scenario 'and cannot with a password confirmation that does not match' do
+    expect { sign_up('mr_wrong@wrong.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+  end
+
   def sign_up(email = 'Guillaume@bouffard.com',
-              password = 'whatever')
+              password = 'whatever',
+              password_confirmation = 'whatever')
     visit '/users/new'
     expect(page.status_code).to eq(200)
     fill_in :email, with: email
     fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
 end
