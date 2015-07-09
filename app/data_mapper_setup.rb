add8594e38e7312046537c5cc55706e1734b4f54
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-07-09 12:02:49 +0100

    Heroku deployment ready

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,9 +1,12 @@
 require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+local_env = "postgres://localhost/chitter_#{env}"
+DataMapper.setup(:default, ENV['DATABASE_URL'] || local_env)
+
 require_relative 'models/peep'
 require_relative 'models/user'
 require_relative 'models/comment'
+
 DataMapper.finalize
 DataMapper.auto_upgrade!
